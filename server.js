1e902db4b7a4af57272e068519405550fe313e9d
Pablo Novas <pjnovas@gmail.com>
2016-08-11 18:48:27 -0300

    added express server

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 const open = require('open');
 
-new WebpackDevServer(webpack(config), config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-  console.log('Opening your system browser...');
-  open('http://localhost:' + config.port + '/webpack-dev-server/');
+var express = require('express');
+var proxy = require('proxy-middleware');
+var url = require('url');
+
+var app = express();
+
+// TODO: Check if isDevelopment and serve statics from 'dist' dir
+app.use('/assets', proxy(url.parse(`http://localhost:${config.port}/assets`)));
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/src/index.html');
 });
+
+new WebpackDevServer(webpack(config), config.devServer)
+  .listen(config.port, 'localhost', (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at localhost:' + config.port);
+    //open('http://localhost:' + config.port + '/webpack-dev-server/');
+    console.log('Opening your system browser...');
+  });
+
+app.listen(8080);
+open('http://localhost:8080');
