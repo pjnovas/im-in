ff3b884321233ffe442b48518627356221170f3e
Pablo Novas <pjnovas@gmail.com>
2016-08-13 15:31:16 -0300

    added express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*eslint no-console:0 */
 'use strict';
+
 require('core-js/fn/object/assign');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
@@ -9,25 +10,34 @@ const open = require('open');
 var express = require('express');
 var proxy = require('proxy-middleware');
 var url = require('url');
+var path = require('path');
 
 var app = express();
+var isDevelopment = process.env !== 'production';
 
-// TODO: Check if isDevelopment and serve statics from 'dist' dir
-app.use('/assets', proxy(url.parse(`http://localhost:${config.port}/assets`)));
+if (isDevelopment){
+  app.use('/assets', proxy(url.parse(`http://localhost:${config.port}/assets`)));
+}
+else {
+  app.use('/assets', express.static('dist'));
+}
 
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/src/index.html');
-});
+if (isDevelopment){
 
-new WebpackDevServer(webpack(config), config.devServer)
-  .listen(config.port, 'localhost', (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log('Listening at localhost:' + config.port);
-    //open('http://localhost:' + config.port + '/webpack-dev-server/');
-    console.log('Opening your system browser...');
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '', '/src/index.html'));
   });
 
-app.listen(8080);
-open('http://localhost:8080');
+  new WebpackDevServer(webpack(config), config.devServer)
+    .listen(config.port, 'localhost', (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Listening at localhost:' + config.port);
+      //open('http://localhost:' + config.port + '/webpack-dev-server/');
+      console.log('Opening your system browser...');
+    });
+
+  app.listen(8080);
+  open('http://localhost:8080');
+}
