2df8adad0b28ef4fb497fe48b917cf93b623a484
Pablo Novas <pjnovas@gmail.com>
2016-08-15 12:31:48 -0300

    updated event form controls

--- src/components/event/Form.js
+++ src/components/event/Form.js
@@ -4,78 +4,55 @@ import { __ } from 'locale';
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import validate from './validator';
-import DateTime from 'react-datetime';
 import Location from './Location';
+
+import { Input, Button, Dropdown, DatePicker, TimePicker } from 'controls';
 
 @reduxForm({
   form: 'event-form',
-  fields: [ 'category', 'title', 'info', 'owner', 'datetime', 'max', 'location' ],
+  fields: [ 'category', 'title', 'info', 'datetime', 'max', 'location' ],
   validate
 })
 export default class FormEvent extends Component {
   render() {
     let {
       categories,
-      fields: { category, title, info, owner, datetime, max, location },
+      fields: { category, title, info, datetime, max, location },
       handleSubmit,
       submitting
     } = this.props;
+
+    let catSource = categories.map( c => {
+      return { value: c, label: __(`cat_${c}`) }
+    });
 
     return (
       <form onSubmit={handleSubmit}>
         <h2>{__('event_create')}</h2>
 
         <div>
-          <label>{__('event_category')}</label>
-          <div>
-            <select>
-              {categories.map(cat =>
-                (<option key={cat} value={cat}>{__(`cat_${cat}`)}</option>)
-              )}
-            </select>
-          </div>
+          <Dropdown auto source={catSource} label={__('event_category')} {...category}/>
           {category.touched && category.error && <div>{category.error}</div>}
         </div>
 
         <div>
-          <label>{__('event_title')}</label>
-          <div>
-            <input type="text" placeholder={__('event_title')} {...title}/>
-          </div>
+          <Input type="text" {...title} label={__('event_title')}/>
           {title.touched && title.error && <div>{title.error}</div>}
         </div>
 
         <div>
-          <label>{__('event_info')}</label>
-          <div>
-            <input type="text" placeholder={__('event_info')} {...info}/>
-          </div>
+          <Input type="text" multiline label={__('event_info')} {...info}/>
           {info.touched && info.error && <div>{info.error}</div>}
         </div>
 
         <div>
-          <label>{__('event_owner')}</label>
-          <div>
-            <input type="text" placeholder={__('event_owner')} {...owner}/>
-          </div>
-          {owner.touched && owner.error && <div>{owner.error}</div>}
-        </div>
-
-        <div>
-          <label>{__('event_date')}</label>
-          <div>
-            <DateTime input={false} {...datetime}
-              dateFormat={__('date_format')} timeFormat={__('time_format')}/>
-            {/*<input type="text" placeholder={__('event_date')} {...datetime}/>*/}
-          </div>
+          <DatePicker label='Cuando' {...datetime}/>
+          <TimePicker label='Hora' {...datetime}/>
           {datetime.touched && datetime.error && <div>{datetime.error}</div>}
         </div>
 
         <div>
-          <label>{__('event_max')}</label>
-          <div>
-            <input type="number" placeholder={__('event_max')} {...max}/>
-          </div>
+          <Input type="number" label={__('event_max')} {...max}/>
           {max.touched && max.error && <div>{max.error}</div>}
         </div>
 
@@ -88,9 +65,9 @@ export default class FormEvent extends Component {
           {location.touched && location.error && <div>{location.error}</div>}
         </div>
 
-        <button type="submit" disabled={submitting}>
+        <Button type="submit" disabled={submitting}>
           {submitting ? <i/> : <i/>} {__('event_submit')}
-        </button>
+        </Button>
       </form>
     );
   }
--- src/controls/index.js
+++ src/controls/index.js
@@ -1,7 +1,15 @@
 import { Link } from 'react-router';
-import { Button } from 'react-toolbox/lib/button';
+import Button from 'react-toolbox/lib/button';
+import Input from 'react-toolbox/lib/input';
+import Dropdown from 'react-toolbox/lib/dropdown';
+import DatePicker from 'react-toolbox/lib/date_picker';
+import TimePicker from 'react-toolbox/lib/time_picker';
 
 export {
   Link,
-  Button
+  Button,
+  Input,
+  Dropdown,
+  DatePicker,
+  TimePicker
 }
